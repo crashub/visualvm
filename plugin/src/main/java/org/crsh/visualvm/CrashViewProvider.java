f5a80e6458d89a29e92470c6f0cd258fcda3ec97
Alain Defrance <adefrance@exoplatform.com>
2012-11-13 17:14:53 +0100

    Prevent visual to deploy crash on itself

--- plugin/src/main/java/org/crsh/visualvm/CrashViewProvider.java
+++ plugin/src/main/java/org/crsh/visualvm/CrashViewProvider.java
@@ -1,9 +1,14 @@
 package org.crsh.visualvm;
 
+import com.sun.tools.attach.AttachNotSupportedException;
+import com.sun.tools.attach.VirtualMachine;
+import com.sun.tools.attach.VirtualMachineDescriptor;
 import com.sun.tools.visualvm.application.Application;
 import com.sun.tools.visualvm.core.ui.DataSourceView;
 import com.sun.tools.visualvm.core.ui.DataSourceViewProvider;
 import com.sun.tools.visualvm.core.ui.DataSourceViewsManager;
+
+import java.io.IOException;
 
 /**
  * @author <a href="mailto:alain.defrance@exoplatform.com">Alain Defrance</a>
@@ -14,6 +19,13 @@ public class CrashViewProvider extends DataSourceViewProvider<Application> {
 
   @Override
   public boolean supportsViewFor(Application application) {
+    System.setProperty("MARKER", "MARKER");
+    try {
+      VirtualMachine vm = VirtualMachine.attach(String.valueOf(application.getPid()));
+      if (vm.getSystemProperties().getProperty("MARKER") != null) {
+        return false;
+      }
+    } catch (Exception e) {}
     return true;
   }
 
